934ff6613169829f72e1fa922880138220b8a2c5
yetemegn-telaye <lemma19yet@gmail.com>
2022-11-11 23:17:19 +0300

    Add tests for both calculate and operate modules

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,11 +1,33 @@
 const calculate = require('../logic/calculate'); // You need to import the file you want to test
 
-test('Performs an operation on numbers and returns an object with result', () => {
-  const obj = {
-    total: '0',
-    next: '2',
-    operation: '+',
-  };
-  const buttonName = '=';
-  expect(calculate(obj, buttonName)).toEqual({ total: '2', next: null, operation: null });
+describe('Perform an operation on numbers and returns an object with result', () => {
+  test('Checks add operation on two numbers then = is clicked', () => {
+    const obj = {
+      total: '0',
+      next: '2',
+      operation: '+',
+    };
+    const buttonName = '=';
+    expect(calculate(obj, buttonName)).toEqual({ total: '2', next: null, operation: null });
+  });
+
+  test('Checks operation on more than 2 numbers', () => {
+    const obj = {
+      total: '0',
+      next: '2',
+      operation: '+',
+    };
+    const buttonName = '+';
+    expect(calculate(obj, buttonName)).toEqual({ total: '2', next: null, operation: '+' });
+  });
+
+  test('Check when buttonName is 0 and next value is 0 and no operation clicked', () => {
+    const obj = {
+      total: '0',
+      next: '0',
+      operation: null,
+    };
+    const buttonName = '=';
+    expect(calculate(obj, buttonName)).toEqual({});
+  });
 });
--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,10 +1,48 @@
 const operate = require('../logic/operate');
 
-test('Performs an operation on three numbers and returns a number result', () => {
-  const obj = {
-    total: '0',
-    next: '2',
-    operation: '+',
-  };
-  expect(operate(obj.total, obj.next, obj.operation)).toBe('2');
+describe('Performs an operation on three numbers and returns a number result', () => {
+  test('Performs addition on 2 numbers', () => {
+    const obj = {
+      total: '0',
+      next: '2',
+      operation: '+',
+    };
+    expect(operate(obj.total, obj.next, obj.operation)).toBe('2');
+  });
+
+  test('Performs multiplication on the numbers', () => {
+    const obj = {
+      total: '0',
+      next: '2',
+      operation: 'x',
+    };
+    expect(operate(obj.total, obj.next, obj.operation)).toBe('0');
+  });
+
+  test('Performs %  on numbers cannot be divided by zero', () => {
+    const obj = {
+      total: '4',
+      next: '0',
+      operation: '%',
+    };
+    expect(operate(obj.total, obj.next, obj.operation)).toBe("Can't find modulo as can't divide by 0.");
+  });
+
+  test('Performs ÷ on numbers cannot be divided by zero', () => {
+    const obj = {
+      total: '8',
+      next: '0',
+      operation: '÷',
+    };
+    expect(operate(obj.total, obj.next, obj.operation)).toBe("Can't divide by 0.");
+  });
+
+  test('Performs - on numbers', () => {
+    const obj = {
+      total: '8',
+      next: '4',
+      operation: '-',
+    };
+    expect(operate(obj.total, obj.next, obj.operation)).toBe('4');
+  });
 });
